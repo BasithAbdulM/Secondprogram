0f121695298b290b738450f2fa9a2b103ec736ae
BasithAbdulM <abdul.basith332@gmail.com>
2022-06-29 09:27:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=10;
+        int c=a+b;
+        System.out.println("Sum of numbers is: "+c);
+
+
+
+
+
+    }
+
+}
